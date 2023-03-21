bc7cfd3bc669b49b961dcfdedda1d404be140d06
Suryasrinivasanv <124970021+Suryasrinivasanv@users.noreply.github.com>
2023-03-21 18:40:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var request = new XMLHttpRequest();
+request.open("GET", "https://restcountries.com/v2/all");
+request.send();
+request.onload = function () {
+  var result = JSON.parse(request.response);
+  console.log(result);
+  for (var i = 0; i < result.length; i++) {
+    console.log(result[i].region);
+    console.log(result[i].flag);
+    if (result[i].population < 200000) {
+      console.log(result[i].region);
+    }
+    console.log(result[i].capital);
+    console.log(result[i].population);
+  }
+};
